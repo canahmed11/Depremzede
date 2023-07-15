12f302b5d029dd6f74c3c5c4f87bb925fb497649
Ahmet URUNVEREN <urunverenahmett@gmail.com>
2023-07-15 18:31:53 +0300

    WhistleFragment created

---
+++ app/src/main/java/com/ahmeturunveren/depremzede/view/WhistleFragment.kt
@@ -0,0 +1,27 @@
+package com.ahmeturunveren.depremzede.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ahmeturunveren.depremzede.R
+
+
+class WhistleFragment : Fragment() {
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_whistle, container, false)
+    }
+
+
+}
