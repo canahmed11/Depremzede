2fd0895629e760b079cdab0eac86e7ccfad4aec1
Ahmet URUNVEREN <urunverenahmett@gmail.com>
2023-07-15 18:24:49 +0300

    view package and adapter package was created

--- app/src/main/java/com/ahmeturunveren/depremzede/view/BagFragment.kt
+++ app/src/main/java/com/ahmeturunveren/depremzede/view/BagFragment.kt
@@ -1,10 +1,11 @@
-package com.ahmeturunveren.depremzede
+package com.ahmeturunveren.depremzede.view
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.ahmeturunveren.depremzede.R
 
 
 class BagFragment : Fragment() {
--- app/src/main/java/com/ahmeturunveren/depremzede/view/MainActivity.kt
+++ app/src/main/java/com/ahmeturunveren/depremzede/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.ahmeturunveren.depremzede
+package com.ahmeturunveren.depremzede.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ahmeturunveren.depremzede.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
